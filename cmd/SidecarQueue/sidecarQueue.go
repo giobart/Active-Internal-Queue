e1ff326b83eb5eeaefcb55bba466ca04ddf7eb94
giobarty <giovanni.bartolomeo.rg@gmail.com>
2023-06-24 21:08:46 +0200

    removed channel size

--- cmd/SidecarQueue/sidecarQueue.go
+++ cmd/SidecarQueue/sidecarQueue.go
@@ -230,7 +230,7 @@ func NextServiceConnect(nextService string) (streamgRPCspec.FramesStreamService_
 		nextService,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithReturnConnectionError(),
-		grpc.WithInitialWindowSize(1024*1024*200),
+		//grpc.WithInitialWindowSize(1024*1024*200),
 	)
 	gRPCclient := streamgRPCspec.NewFramesStreamServiceClient(clientConn)
 	var stream streamgRPCspec.FramesStreamService_StreamFramesClient = nil
@@ -256,7 +256,7 @@ func ReceiveFrameGrpcRoutine() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer(grpc.MaxRecvMsgSize(100*102*1024), grpc.MaxSendMsgSize(1024*1024*100)) //100MB max size
+	s := grpc.NewServer() //grpc.MaxRecvMsgSize(100*102*1024), grpc.MaxSendMsgSize(1024*1024*100)) //100MB max size
 	streamgRPCspec.RegisterFramesStreamServiceServer(s, StreamServer{})
 	if err := s.Serve(serverListener); err != nil {
 		log.Println("gRPC server error")
